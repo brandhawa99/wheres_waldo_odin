454a4532b419e303c66a0cdad22c53a6816cee98
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-02-16 22:37:04 -0800

    moved function to find coords into app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 
+import React, {useState, useEffect} from 'react'
 import './App.css';
 import Header from './components/Header';
 import Game from './components/Game'
+import dog from '../src/components/img/dog.jpg'
+import hat from '../src/components/img/guyInHat.jpg'
+import guy from '../src/components/img/blackHatBlueshirt.jpg'
+
+
 function App() {
+
+  const [bottomA, setBottomA]  = useState([
+    {
+      name: 'cowboy hat',
+      x:215.5, 
+      y:833, 
+      found: false
+    }
+  ])
+
+  const getCordinatesOfClick = (e) =>{
+    let WINDOW_SIZE = window.innerWidth;
+    let xCord = null;
+
+    if(WINDOW_SIZE>600){
+        xCord = Math.abs(e.pageX-((WINDOW_SIZE/2) - (300)))
+    }else{
+        xCord = e.pageX;
+    }
+    console.log('xCord:', xCord, 'yCord:', e.pageY )
+    console.log("  ");
+}
+
   return (
     <div className='container'>
-      <Header/>
+      <Header dog={dog} guy={guy} hat={hat}/>
       <div className='main-container'>
-        <Game />
+        <Game click={getCordinatesOfClick}/>
       </div>
 
     </div>
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,40 +1,13 @@
-import React, { useRef } from 'react';
 
 import themePark from './img/themePark.jpg'
 
 // 600x796
 
 
-const Game  = prpos =>{
-    const image = useRef();
-    
-    const getCordinates = (e) =>{
-        let WINDOW_SIZE = window.innerWidth;
-        let imgWidth = image.current.clientWidth;
-        let xCord = null;
-
-        console.log('x:', e.pageX , 'y:',e.pageY, 'current Corrdinates');
-        // console.log(window.innerWidth/2 , 'inner Width');
-        // console.log('Client Width:', image.current.clientWidth/2)
-
-        if(WINDOW_SIZE>600){
-            xCord = Math.abs(e.pageX-((WINDOW_SIZE/2) - (imgWidth/2)))
-        }else{
-            xCord = e.pageX;
-        }
-
-
-
-        console.log('ActualSpot x coord:', xCord )
-        console.log('ActualSpot y coord:', e.pageY )
-        console.log("  ");
-
-    }
-
-
+const Game  = props =>{
     return(
         <div className='game'> 
-            <img ref={image} onClick={getCordinates} src={themePark} alt='theme park' />
+            <img onClick={props.click} src={themePark} alt='theme park' />
         </div>
     )
 

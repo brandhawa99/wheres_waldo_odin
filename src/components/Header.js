addd429d010f560e5b4a7c9b62df52e1108c2660
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-02-16 22:37:15 -0800

    added images to find to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,18 @@ import React from 'react';
 import '../App.css'
 
 
-const Header = ()  =>{
+const Header = (props)  =>{
     
     return(
         <div className='Header'>
             <h1>
                 - Where's Waldo -
             </h1>
+            <div className='pics'>
+                <img src={props.dog} alt='dog'/>
+                <img src={props.guy} alt='guy' />
+                <img src={props.hat} alt='hat'/>
+            </div>
         </div>
     )
 

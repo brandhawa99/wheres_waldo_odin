590eac1e64bbd8b9c5e68324a4c7a853fd168ec8
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-02-24 12:31:54 -0800

    asd'

--- src/components/WinScreen.js
+++ src/components/WinScreen.js
@@ -1,8 +1,9 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css'
 
 const WinScreen = props =>{
+    const [added, setAdded] = useState(false);
 
 
     return(
